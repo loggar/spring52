08ae9b05db86e476fb473c8a345793c2faa3c11f
Charly LEE <charly.loggar@gmail.com>
2020-03-17 15:11:20 +1100

    feat (@Value) types and default values

---
+++ src/main/java/com/loggar/annotation/value/env/ValueDefaultsConfig.java
@@ -0,0 +1,33 @@
+package com.loggar.annotation.value.env;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ValueDefaultsConfig {
+	// cp.user.active=true
+	@Value("${cp.user.active}") private Boolean isUserActive;
+	@Value("${cp.user.active:true}") private Boolean isUserActiveWithDefault;
+
+	// cp.user.age=50
+	@Value("${cp.user.age}") private Integer userAge;
+	@Value("${cp.user.age:30}") private Integer userAgeWithDefault;
+
+	// cp.user.skills=Java, Spring, Angular
+	@Value("${cp.user.skills}") private String[] userSkills;
+	@Value("${cp.user.skills:Java,Spring,Angular}") private String[] userSkillsWithDefault;
+
+	// cp.user.skillids=1,2,3
+	@Value("${cp.user.skillids:5,6,7}") private int[] userSkillIds;
+
+	@Value("#{'${cp.user.skills}'.split(',')}") private List<String> userSkillsList;
+	@Value("#{'${cp.user.skills:Java,HTML,Spring}'.split(',')}") private List<String> userSkillsWithDefatult;
+
+	// cp.user.teamMates={100: 'Krishna', 200: 'Shiva'}
+	@Value("#{${cp.user.teamMates}}") private Map<Integer, String> teamMates;
+	@Value("#{${cp.user.teamMates: {100: 'Krishna', 200: 'Shiva'}}}") private Map<Integer, String> teamMatesWithDefault;
+
+}
--- src/main/java/com/loggar/annotation/value/env/ValueTypesConfig.java
+++ src/main/java/com/loggar/annotation/value/env/ValueTypesConfig.java
@@ -1,8 +1,10 @@
 package com.loggar.annotation.value.env;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
-public class ValueDefaults {
+@Configuration
+public class ValueTypesConfig {
 	// Default Value
 	@Value("Default Value") private String defaultName;
 	@Value("true") private boolean defaultBoolean;
@@ -19,6 +21,8 @@ public class ValueDefaults {
 	// SpEL
 	@Value("#{systemProperties['java.home']}") private String javaHome2;
 
+	@Value("#{systemProperties['unknown_prop_key'] ?: 'Default System Value'}") private String mySystemVal;
+
 	// Spring @Value with methods
 	@Value("Test")
 	public void printValues(String s, String v) {
@@ -28,4 +32,5 @@ public class ValueDefaults {
 	public void printValues2(String s, @Value("Data") String v) {
 	}
 	// s=Test, v=Data
+
 }
