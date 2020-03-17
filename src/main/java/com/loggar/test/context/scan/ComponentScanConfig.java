3a4fec5e7b7480d830db7dd4817cae18e5303c73
Charly LEE <charly.loggar@gmail.com>
2020-03-17 15:35:27 +1100

    refactor (package)

--- src/main/java/com/loggar/test/context/scan/ComponentScanConfig.java
+++ src/main/java/com/loggar/test/context/scan/ComponentScanConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = { "com.loggar.test.config" })
+@ComponentScan(basePackages = { "com.loggar.test.context.scan" })
 public class ComponentScanConfig {
 
 }
